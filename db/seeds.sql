5639a7f4e5808acf3522242cff3220c467a9308a
Brian Done <briguy2078@gmail.com>
2022-01-17 19:16:47 -0500

    updated schema and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO department (name)
 VALUES
-  ('Sales');
-  ('Engineering');
-  ('Finance');
+  ('Sales'),
+  ('Engineering'),
+  ('Finance'),
   ('Legal');
 
 INSERT INTO role (title, salary, department_id)
@@ -14,20 +14,20 @@ VALUES
   ('Account Manager', 160000, 3),
   ('Accountant', 125000, 3),
   ('Legal Team Lead', 250000, 4),
-  ('Lawyer', 190000, 4)
+  ('Lawyer', 190000, 4);
   
 
   INSERT INTO employee (first_name, last_name, role_id, manager_id)
   VALUES
-  ('John', 'Doe', 1, 'null'),
-  ('Mike', 'Chan', 2, 'John Doe'),
-  ('Ashley', 'Rodriguez', 3, 'null'),
-  ('Kevin', 'Tupik', 4, 'Ashley Rodriquez'),
-  ('Kunal', 'Singh', 5, 'null'),
-  ('Malia', 'Brown', 6, 'Kunal Singh'),
-  ('Sarah', 'Lourd', 7, 'null'),
-  ('Tom', 'Allen', 8, 'Sarah Lourd'),
-  ('Sam', 'Kash', 1, 'Ashley Rodriguez')
+  ('John', 'Doe', 1, null),
+  ('Mike', 'Chan', 2, 1),
+  ('Ashley', 'Rodriguez', 3, null),
+  ('Kevin', 'Tupik', 4, 3),
+  ('Kunal', 'Singh', 5, null),
+  ('Malia', 'Brown', 6, 5),
+  ('Sarah', 'Lourd', 7, null),
+  ('Tom', 'Allen', 8, 7),
+  ('Sam', 'Kash', 1, 3);
   
   
  
